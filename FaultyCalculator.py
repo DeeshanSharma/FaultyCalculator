b9347d32ac1877e43f387aa53e3d3ef8b1ee31e2
Deeshan Sharma <42265709+DeeshanSharma@users.noreply.github.com>
2020-01-01 14:09:17 +0530

    Update FaultyCalculator.py
    It is a faulty calculator it will produce correct results of expressions except these
    # 45 * 3 = 555, 56 + 9 = 77, 56 / 6 = 4

--- FaultyCalculator.py
+++ FaultyCalculator.py
@@ -3,25 +3,25 @@
 #Your program should take operator and two numbers as input from the user and then return the result
 
 
-op1 = int(input("Enter operator 1 = "))   #Operator no 1
-op2 = int(input("Enter operator 2 = "))   #Operator no 2
-op = input("Enter the operation (+, -, *, /)= ")    #Operation to be performed
+op1 = int(input("Enter operator 1 = "))   # Operator no 1
+op2 = int(input("Enter operator 2 = "))   # Operator no 2
+op = input("Enter the operation (+, -, *, /)= ")    # Operation to be performed
 
-exp = [op1, op, op2]    #Expression created using above to be matched with given problems
+exp = [op1, op, op2]    # Expression created using above to be matched with given problems
 
-#Checking for first expression
+# Checking for first expression
 if (exp == [45, '*', 3]):
     print(555)
 
-#Checking for second expression
+# Checking for second expression
 elif (exp == [56, '+', 9]):
     print(77)
 
-#Checking for third expression
+# Checking for third expression
 elif (exp == [56, '/', 6]):
     print("4")
 
-#Correct calculations further :-
+# Correct calculations further :-
 elif (op == '+'):
     print(op1 + op2)
 
