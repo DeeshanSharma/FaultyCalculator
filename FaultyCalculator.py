1db238206f576e113c70c2c19cfdd23dd2e8a2dc
shitalsakariya <47467256+shitalsakariya@users.noreply.github.com>
2020-10-01 13:17:04 +0530

    Update FaultyCalculator.py

--- FaultyCalculator.py
+++ FaultyCalculator.py
@@ -2,7 +2,7 @@
 # 45 * 3 = 555, 56 + 9 = 77, 56 / 6 = 4
 # Your program should take operator and two numbers as input from the user and then return the result
 
-
+print("---------------Welcome to the faulty calculator---------------------")
 First_no = eval(input("Enter First no. = "))  # it takes the first no.
 Second_no = eval(input("Enter Second no. = "))  # it takes the second no.
 operator_choice = input("Enter the operation (+, -, *, /)= ")  # Operation selection
@@ -13,15 +13,15 @@ exp = [First_no, operator_choice, Second_no]  # Expression created using above t
 
 #  first faulty expression
 if exp == [45, '*', 3]:
-    print(555)
+    print("The answer is",555)
 
 # second faulty expression
 elif exp == [56, '+', 9]:
-    print(77)
+    print("The answer is",77)
 
 # third faulty expression
 elif exp == [56, '/', 6]:
-    print("4")
+    print("The answer is","4")
 
 # Correct calculations  :-
 elif operator_choice == '+':
