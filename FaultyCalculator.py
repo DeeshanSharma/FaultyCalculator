ad85f7727b37091bc867e2593af965f9153d8a1e
anmolbatth <72181178+anmolbatth@users.noreply.github.com>
2020-10-01 10:14:06 +0530

    Update FaultyCalculator.py

--- FaultyCalculator.py
+++ FaultyCalculator.py
@@ -1,3 +1,4 @@
+#codewithharry
 #Design a calculator which will correctly solve all the problems except the following ones:
 #45 * 3 = 555, 56 + 9 = 77, 56 / 6 = 4
 #Your program should take operator and two numbers as input from the user and then return the result
